2869bb1f26d0efd203465a0653735461ad28f2dd
Łukasz Węgiel <lukasz@mirumee.com>
2017-03-27 12:20:02 +0200

    Add TaskForm component

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
-import { StyleSheet, View } from 'react-native'
+import { Navigator, StyleSheet, View } from 'react-native'
 import TaskList from './TaskList'
+import TaskForm from './TaskForm'
 
 export default class App extends Component {
   constructor (props, context) {
@@ -17,13 +18,57 @@ export default class App extends Component {
     }
   }
 
+  _onAddStarted = () => {
+    this.nav.push({
+      'name': 'taskform'
+    })
+  }
+
+  _renderScene = (route, nav) => {
+    switch (route.name) {
+      case 'taskform':
+        return (
+          <TaskForm
+            onAdd={this._onAdd}
+            onCancel={this._onCancel}
+          />
+        )
+      default:
+      return (
+        <View style={styles.container}>
+          <TaskList
+            onAddStarted={this._onAddStarted}
+            todos={this.state.todos}
+          />
+        </View>
+      )
+    }
+  }
+
+  _configureScene = () => {
+    return Navigator.SceneConfigs.FloatFromBottom
+  }
+
+  _onAdd = (task) => {
+    this.state.todos.push({ task })
+    this.setState({ todos: this.state.todos })
+    this.nav.pop()
+  }
+
+  _onCancel = () => {
+    this.nav.pop()
+  }
+
   render () {
     return (
-      <View style={styles.container}>
-        <TaskList
-          todos={this.state.todos}
-        />
-      </View>
+      <Navigator
+        configureScene={this._configureScene}
+        initialRoute={{ name: 'tasklist', index: 0 }}
+        ref={((nav) => {
+          this.nav = nav
+        })}
+        renderScene={this._renderScene}
+      />
     )
   }
 }
--- TaskList.js
+++ TaskList.js
@@ -1,5 +1,5 @@
 import React, { Component, PropTypes } from 'react'
-import { ListView, StyleSheet, View } from 'react-native'
+import { ListView, StyleSheet, TouchableHighlight, Text, View } from 'react-native'
 import TaskRow from './TaskRow'
 
 export default class TaskList extends Component {
@@ -13,6 +13,11 @@ export default class TaskList extends Component {
     this.state = {
       dataSource: ds.cloneWithRows(props.todos)
     }
+  }
+
+  componentWillReceiveProps (nextProps) {
+    const dataSource = this.state.dataSource.cloneWithRows(nextProps.todos)
+    this.setState({ dataSource })
   }
 
   _renderRow = (todo) => {
@@ -31,12 +36,19 @@ export default class TaskList extends Component {
           key={this.props.todos}
           renderRow={this._renderRow}
         />
+        <TouchableHighlight
+          onPress={this.props.onAddStarted}
+          style={styles.button}
+        >
+          <Text style={styles.buttonText}>Add one</Text>
+        </TouchableHighlight>
       </View>
     )
   }
 }
 
 TaskList.propTypes = {
+  onAddStarted: PropTypes.func.isRequired,
   todos: PropTypes.arrayOf(PropTypes.object).isRequired
 }
 
@@ -46,5 +58,19 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'flex-start',
     paddingTop: 30
+  },
+  button: {
+    alignItems: 'center',
+    backgroundColor: '#333',
+    borderColor: '#05a0d1',
+    borderWidth: 2,
+    height: 60,
+    justifyContent: 'center',
+    margin: 20
+  },
+  buttonText: {
+    color: '#fafafa',
+    fontSize: 20,
+    fontWeight: '600'
   }
 })
